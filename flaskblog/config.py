89afaad4c8ee94430ceb1b24e199065e28bec722
Vignesh Viswanathan <53799125+vigviswa@users.noreply.github.com>
2020-12-31 14:23:27 -0600

    Removed Database URi

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,4 +2,3 @@ class Config:
     SECRET_KEY = "0d2e81519f6420437ca98c44c704bf6a"
     SQLALCHEMY_TRACK_MODIFICATIONS = "False"
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    # SQLALCHEMY_DATABASE_URI = "mysql://admin:gundusaarav@flask-mysql.csw0rdijz2bq.us-east-1.rds.amazonaws.com/flask"
