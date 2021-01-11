8592d2d0301b94315c63c9d389e0856c995f5549
Vignesh Viswanathan <53799125+vigviswa@users.noreply.github.com>
2021-01-11 16:03:23 -0600

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,6 +2,9 @@ class Config:
     SECRET_KEY = "0d2e81519f6420437ca98c44c704bf6a"
     SQLALCHEMY_TRACK_MODIFICATIONS = "False"
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    # SQLALCHEMY_DATABASE_URI = (
+    #     "mysql://admin:Gundusaarav@1234@127.0.0.1:{}/flask".format(tunnel)
+    # )
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
