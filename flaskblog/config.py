e4efc13c308b7f38bc3cc82fba88dab5ce70c659
Vignesh Viswanathan <vig.viswa@gmail.com>
2021-01-14 01:54:12 -0600

    Final Demo Version v.2

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,12 +1,8 @@
 class Config:
     SECRET_KEY = "0d2e81519f6420437ca98c44c704bf6a"
     SQLALCHEMY_TRACK_MODIFICATIONS = "False"
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    # SQLALCHEMY_DATABASE_URI = (
-    #     "mysql://admin:Gundusaarav@1234@127.0.0.1:{}/flask".format(tunnel)
-    # )
-    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_SERVER = "smtp.email.us-phoenix-1.oci.oraclecloud.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = ""
-    MAIL_PASSWORD = ""
+    MAIL_USERNAME = "ocid1.user.oc1..aaaaaaaaxkpb5pjfaf4432pd4fb7vsjzbprdghzyfxog56w6xg4lcio6qhga@ocid1.tenancy.oc1..aaaaaaaary5pyagidupdq63x5fyeo4oenxt7tvtjtrpp6sljbmxslz56liza.of.com"
+    MAIL_PASSWORD = "f<p3mZ1Pw4jSz(mMYPmW"
--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -38,7 +38,7 @@ def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message(
         "Password Reset Request",
-        sender="noreply@flaskblog.com",
+        sender="vigviswa@cloudmazingblog.com",
         recipients=[user.email],
     )
     msg.body = f"""To reset your password, visit the following link:
