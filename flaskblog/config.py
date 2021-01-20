ad6024746ddde7005af7be969f01d6e421c4def4
Vignesh Viswanathan <53799125+vigviswa@users.noreply.github.com>
2021-01-20 13:13:00 -0600

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,8 @@
 class Config:
-    SECRET_KEY = "0d2e81519f6420437ca98c44c704bf6a"
+    SECRET_KEY = "{SECRET_KEY}"
     SQLALCHEMY_TRACK_MODIFICATIONS = "False"
     MAIL_SERVER = "smtp.email.us-phoenix-1.oci.oraclecloud.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "ocid1.user.oc1..aaaaaaaaxkpb5pjfaf4432pd4fb7vsjzbprdghzyfxog56w6xg4lcio6qhga@ocid1.tenancy.oc1..aaaaaaaary5pyagidupdq63x5fyeo4oenxt7tvtjtrpp6sljbmxslz56liza.of.com"
-    MAIL_PASSWORD = "f<p3mZ1Pw4jSz(mMYPmW"
+    MAIL_USERNAME = "{USERNAME}"
+    MAIL_PASSWORD = "{PASSWORD}"
